fixed masking issues where tymask ping and a bug were a fixed

Dataset.py
@@ -106,6 +106,8 @@ class RNADataset(Dataset):
 
 class TestRNAdataset(RNADataset):
     def __getitem__(self, idx):
+        
+        idx=self.indices[idx]
 
         #id=self.ids[idx]
 
@@ -135,11 +137,12 @@ class Custom_Collate_Obj:
 
 
     def __call__(self,data):
+        # 
         length=[]
         for i in range(len(data)):
             length.append(len(data[i]['sequence']))
         max_len=max(length)
-
+        max_len=206
 
         sequence=[]
         labels=[]
@@ -152,9 +155,11 @@ class Custom_Collate_Obj:
         #print(data['bpp'])
         if use_bpp:
             bpps=[]
+        length=[]
         for i in range(len(data)):
-            to_pad=max_len-length[i]
-
+            #to_pad=max_len-length[i]
+            to_pad=max_len-len(data[i]['sequence'])
+            length.append(len(data[i]['sequence']))
             #if to_pad>0:
             sequence.append(F.pad(data[i]['sequence'],(0,to_pad),value=4))
             #masks.append(data[i]['mask'])
@@ -205,7 +210,8 @@ class Custom_Collate_Obj_test(Custom_Collate_Obj):
         use_bpp='bpp' in data[0]
         if use_bpp:
             bpps=[]
-        max_len=max(length)
+        max_len=max(length)#+100
+        #max_len=206
         sequence=[]
         masks=[]
         for i in range(len(data)):

Network.py
@@ -69,10 +69,22 @@ class ScaledDotProductAttention(nn.Module):
 
 
         if attn_mask is not None:
-            attn=attn.float().masked_fill(attn_mask == -1, float('-1e-9'))
-
-
+            attn=attn.float().masked_fill(attn_mask == -1, float('-1e9'))
+        # print(attn_mask.shape)
+        # print(attn_mask)
+        # print(attn[0,0])
+        # exit()
         attn = self.dropout(F.softmax(attn, dim=-1))
+
+        # if attn_mask is not None:
+        #     attn=attn.float().masked_fill(attn_mask == -1, 0.0)
+
+        # print(attn.shape)
+        # plt.imshow(attn[0,0].detach().cpu(),vmin=0)
+        # plt.savefig('attn.png',dpi=500)
+        # exit()
+        # plt.imshow(attn)
+
         # print(attn[0,0])
         # to_plot=attn[0,0].detach().cpu().numpy()
         # with open('mat.txt','w+') as f:
@@ -131,9 +143,11 @@ class MultiHeadAttention(nn.Module):
         # print(k.shape)
         # print(v.shape)
         if src_mask is not None:
+            src_mask=src_mask.clone().unsqueeze(-1).long()
             src_mask[src_mask==0]=-1
-            src_mask=src_mask.unsqueeze(-1).float()
-            attn_mask=torch.matmul(src_mask,src_mask.permute(0,2,1)).unsqueeze(1)
+            src_mask=src_mask.float()
+            #src_mask=src_mask.unsqueeze(-1)#.float()
+            attn_mask=torch.matmul(src_mask,src_mask.permute(0,2,1)).unsqueeze(1).long()
             q, attn = self.attention(q, k, v, mask=mask,attn_mask=attn_mask)
         else:
             q, attn = self.attention(q, k, v, mask=mask)
@@ -204,7 +218,7 @@ class ConvTransformerEncoderLayer(nn.Module):
         self.pair_transition=nn.Sequential(
                                            nn.LayerNorm(pairwise_dimension),
                                            nn.Linear(pairwise_dimension,pairwise_dimension*4),
-                                           nn.ReLU(inplace=True),
+                                           nn.ReLU(inplace=False),
                                            nn.Linear(pairwise_dimension*4,pairwise_dimension))
 
 
@@ -346,6 +360,12 @@ class TriangleMultiplicativeModule(nn.Module):
     def forward(self, x, src_mask = None):
         src_mask=src_mask.unsqueeze(-1).float()
         mask = torch.matmul(src_mask,src_mask.permute(0,2,1))
+
+        # print(mask.shape)
+        # plt.imshow(mask[0].detach().cpu())
+        # plt.savefig('mask.png')
+        # exit()
+
         assert x.shape[1] == x.shape[2], 'feature map must be symmetrical'
         if exists(mask):
             mask = rearrange(mask, 'b i j -> b i j ()')